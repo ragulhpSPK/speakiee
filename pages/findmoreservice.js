ba9de5135b0305c0a1c265e288e15f7981de90c7
ragul-spk <ragulhp27@gmail.com>
2023-03-03 12:08:31 +0530

    edit001

--- pages/findmoreservice.js
+++ pages/findmoreservice.js
@@ -96,29 +96,190 @@ function Frontend() {
         </div>
       </div>
 
-
       <div className={styles.second_containr}>
         <div className={styles.secondcon_firstcontent}>
-          <div>
+          <div className={styles.second_con_img}>
             <Image
               src="/assets/images/content.png"
-              width={300}
+              width={400}
               height={400}
               alt="no image to display"
             />
           </div>
-          <div>
+          <span className={styles.border}></span>
+          <div className={styles.secondcon_para}>
             <h1>
               {Findmorecontentpage.map((res) => {
                 return res.Heading5;
               })}
             </h1>
             <p>
-              {" "}
               {Findmorecontentpage.map((res) => {
                 return res.content5;
               })}
             </p>
+          </div>
+        </div>
+
+        <div className={styles.secondcon_secondContent}>
+          <div className={styles.secondcontainer_img}>
+            <div className={styles.secondcon_img1}>
+              <Image
+                src="/assets/images/front.png"
+                alt="no image to display"
+                width={300}
+                height={400}
+              />
+            </div>
+
+            <div className={styles.secondcon_img2}>
+              <Image
+                src="/assets/images/front.png"
+                alt="no image to display"
+                width={300}
+                height={400}
+              />
+            </div>
+
+            <div className={styles.secondcon_img3}>
+              <Image
+                src="/assets/images/front.png"
+                alt="no image to display"
+                width={300}
+                height={400}
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className={styles.secondcon_thirdcontent}>
+          <div className={styles.secondcon_thirdcontent_para1}>
+            <p>
+              {Findmorecontentpage.map((res) => {
+                return res.content6;
+              })}
+            </p>
+          </div>
+
+          <div className={styles.secondcon_thirdcontent_para2}>
+            <p>
+              {Findmorecontentpage.map((res) => {
+                return res.content7;
+              })}
+            </p>
+          </div>
+
+          <div className={styles.secondcon_thirdcontent_para3}>
+            <p>
+              {Findmorecontentpage.map((res) => {
+                return res.content8;
+              })}
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div className={styles.third_container}>
+        <div className={styles.third_container_firstpage}>
+          <div className={styles.third_container_para}>
+            <h1>
+              {Findmorecontentpage.map((res) => {
+                return res.Heading9;
+              })}
+            </h1>
+            <p>
+              {Findmorecontentpage.map((res) => {
+                return res.content9;
+              })}
+            </p>
+          </div>
+          <span className={styles.border} />
+          <div className={styles.third_container_img}>
+            <Image
+              src="/assets/images/frontend.jpg"
+              alt="no Image to display"
+              height={400}
+              width={400}
+            />
+          </div>
+        </div>
+        <div className={styles.third_container_secondpage}>
+          <div className={styles.third_container_secondpage_para1}>
+            <p>
+              {Findmorecontentpage.map((res) => {
+                return res.content10;
+              })}
+            </p>
+          </div>
+          <div className={styles.third_container_secondpage_para2}>
+            <p>
+              {Findmorecontentpage.map((res) => {
+                return res.content11;
+              })}
+            </p>
+          </div>
+          <div className={styles.third_container_secondpage_para3}>
+            <p>
+              {Findmorecontentpage.map((res) => {
+                return res.content12;
+              })}
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div className={styles.fourth_container}>
+        <div className={styles.fourcon_content}>
+          <div className={styles.fourcon_content_para1}>
+            <p>
+              {Findmorecontentpage.map((res) => {
+                return res.content13;
+              })}
+            </p>
+          </div>
+          <div className={styles.fourcon_content_para2}>
+            <p>
+              {Findmorecontentpage.map((res) => {
+                return res.content14;
+              })}
+            </p>
+          </div>
+          <div className={styles.fourcon_content_para3}>
+            <p>
+              {Findmorecontentpage.map((res) => {
+                return res.content15;
+              })}
+            </p>
+          </div>
+          <div className={styles.fourcon_content_para4}>
+            <p>
+              {Findmorecontentpage.map((res) => {
+                return res.content16;
+              })}
+            </p>
+          </div>
+        </div>
+
+        <div className={styles.fourcon_second_content}>
+          <div className={styles.fourcon_second_content_para1}>
+            <h1>
+              {Findmorecontentpage.map((res) => {
+                return res.Heading17;
+              })}
+            </h1>
+            <p>
+              {Findmorecontentpage.map((res) => {
+                return res.content17;
+              })}
+            </p>
+          </div>
+          <div className={styles.fourcon_img}>
+            <Image
+              src="/assets/images/front.png"
+              width={300}
+              height={300}
+              alt="no image to display"
+            />
           </div>
         </div>
       </div>
